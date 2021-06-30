adb1e1216957df1ba81f28a7aedb4ccea089ffc8
Satyam Mandal <satyam.mandal.77@gmail.com>
2021-06-30 22:58:01 +0530

    task 3 Complete

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
-function loading(){	
-	var loader=document.getElementById('loading').classList;
-	loader.remove('hide');
-	setTimeout(function(){loader.add('hide')},2000);
+
+function loading() {
+	if (document.getElementById('loader').disabled != true) {
+		var loader = document.getElementById('submit').classList;
+		var icon = document.getElementById('loader').classList;
+		icon.add('load');
+		icon.add('icon');
+		//change color variable value
+		setTimeout(function () {
+			//loader.remove('input-field');
+			icon.remove('load');
+			icon.remove('icon');
+			loader.remove('submit');
+			loader.add('submit-disable');
+			document.getElementById('loader').disabled = true;
+
+		}, 2000);
+	}
 }
